22c3cf527e059497f6a2a88402cc811a7263eac4
JBrinco <j.brinco@campus.fct.unl.pt>
2021-01-06 14:12:35 +0000

    modified:   README.md 	modified:   main.r

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ A simple R script that performs a calibration with external standardization (sol
 ## Development
 
 - [ ] Basic functionality with one csv input of calibration and another of sample signal values. Outputs results to CSV.
+- [ ] Outputs good looking calibration curves with formulas to png or gif or svg (ideal).
 - [ ] Can process multiple calibrations at once, and one single sample signal file with values for all calibrations.
 - [ ] Can read columns and decide if internal standard was used or not.
 - [ ] Fancy hackable output with Rmarkdown.
--- main.r
+++ main.r
@@ -4,7 +4,24 @@ args = commandArgs(trailingOnly=TRUE)
 #On first run uncomment this line!
 #install.packages('pacman', repos='http://cran.us.r-project.org')
 
-pacman::p_load(pacman, ggplot2, dplyr, broom, ggpubr)
+#pacman::p_load(pacman, ggplot2, dplyr, broom, ggpubr)
 
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+	stop("Not enough Arguments! \n Usage: Rscript qbuddy.r [calibration_data.csv] [Sample_signal_data.csv] [(OPTIONAL)output_file.csv]", call.=FALSE)
+} else if (length(args)==1) {
+	stop("Not enough Arguments! \n Usage: Rscript qbuddy.r [calibration_data.csv] [Sample_signal_data.csv] [(OPTIONAL)output_file.csv]", call.=FALSE)
+} else{
+# default output file
+  args[2] = "out.txt"
+}
 
 print(args)
+
+calibration = read.csv(args[1], header=TRUE)
+
+print(calibration)
+
+#Calculation
+
+summary(calibration)
